ed2f5542bc8ab4f6a52b92af67fe3741d0bda999
miguelwolf <miguelwolf@gaudium.com.br>
2026-04-20 09:52:56 -0300

    fix: Alterado para ruas reais

--- lib/test/data/data_source/historico_corridas_api_response_json_mock.dart
+++ lib/test/data/data_source/historico_corridas_api_response_json_mock.dart
@@ -1,37 +1,39 @@
 const Map<String, dynamic> historicoCorridasApiResponseJsonMock = {
   'success': true,
   'response': {
-    'corridas': [
-      {
-        'id': 1,
-        'endereco_origem': 'Rua 1',
-        'endereco_destino': 'Rua 2',
-        'data': '2022-01-01',
-      },
-      {
-        'id': 2,
-        'endereco_origem': 'Rua 3',
-        'endereco_destino': 'Rua 4',
-        'data': '2022-01-01',
-      },
-      {
-        'id': 3,
-        'endereco_origem': 'Rua 5',
-        'endereco_destino': 'Rua 6',
-        'data': '2022-01-01',
-      },
-      {
-        'id': 4,
-        'endereco_origem': 'Rua 7',
-        'endereco_destino': 'Rua 8',
-        'data': '2022-01-01',
-      },
-      {
-        'id': 5,
-        'endereco_origem': 'Rua 9',
-        'endereco_destino': 'Rua 10',
-        'data': '2022-01-01',
-      },
-    ],
+    {
+      "corridas": [
+        {
+          "id": 1,
+          "endereco_origem": "Avenida Paulista, 1000",
+          "endereco_destino": "Rua Augusta, 1500",
+          "data": "2022-01-01",
+        },
+        {
+          "id": 2,
+          "endereco_origem": "Rua Oscar Freire, 500",
+          "endereco_destino": "Avenida Brigadeiro Faria Lima, 2232",
+          "data": "2022-01-01",
+        },
+        {
+          "id": 3,
+          "endereco_origem": "Praça da Sé, 1",
+          "endereco_destino": "Rua 25 de Março, 100",
+          "data": "2022-01-01",
+        },
+        {
+          "id": 4,
+          "endereco_origem": "Avenida Ipiranga, 200",
+          "endereco_destino": "Avenida São João, 400",
+          "data": "2022-01-01",
+        },
+        {
+          "id": 5,
+          "endereco_origem": "Rua dos Pinheiros, 700",
+          "endereco_destino": "Avenida Europa, 150",
+          "data": "2022-01-01",
+        },
+      ],
+    },
   },
 };
